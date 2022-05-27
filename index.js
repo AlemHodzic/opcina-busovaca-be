308bef02f89871c1ffb7bf99abc17e95ccf002f9
Alem <alem.hodzic1999@gmail.com>
2022-05-28 00:45:48 +0200

    cors

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(cors());
 app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'https://www.opcina-busovaca.com/');
+    res.setHeader('Access-Control-Allow-Origin', 'https://www.opcina-busovaca.com');
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
